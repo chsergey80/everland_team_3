d373b5b85e2e9695079ce41f6160b7193307a1cc
Ainur <mr.ainur.02@mail.ru>
2023-01-14 17:28:35 +0500

    тест коммита

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*Нахождение всех нужных для взаимодействия элементов*/
+const popup = document.querySelector(".popup");
+const menuButton = document.querySelector(".header-navigation__menu");
+const menuImg = document.querySelector(".header-navigation__menu-img")
+
+const mobileList = document.querySelectorAll(".popup__list");
+const mobileMenuList = document.querySelectorAll(".popup__button");
+const mobileButtonImg = document.querySelectorAll(".popup__button-img");
+const mobileMenu = document.querySelectorAll(".popup__list-items");
+
+/*Открытие попапа*/
+function openPopup(item) {
+    item.classList.toggle("popup_opened");
+}
+
+function openMobilePopup(item) {
+    item.classList.toggle("popup__list-items_opened");
+}
+
+menuButton.addEventListener("click", function () {
+    menuButton.classList.toggle("header-navigation__menu_opened");
+    openPopup(popup)
+})
