b276aae02d012b3a9d878b4d0fab9e45911dc020
Ainur <mr.ainur.02@mail.ru>
2023-01-16 13:40:39 +0500

    Сделано меню для мобильных устройств

--- js/index.js
+++ js/index.js
@@ -3,8 +3,18 @@ const popup = document.querySelector(".popup");
 const menuButton = document.querySelector(".header-navigation__menu");
 const menuImg = document.querySelector(".header-navigation__menu-img")
 
-const mobileList = document.querySelectorAll(".popup__list");
-const mobileMenuList = document.querySelectorAll(".popup__button");
+/*Нахождение элементов для мобильнеых устройств*/
+const mobileMenuProject = document.querySelector(".popup__list-project");
+const menuProject = mobileMenuProject.querySelector(".popup__button");
+const menuProjectBlock = mobileMenuProject.querySelector(".popup__list-items");
+
+const mobileMenuPeople = document.querySelector(".popup__list-people");
+const menuPeople = mobileMenuPeople.querySelector(".popup__button");
+const menuPeopleBlock = mobileMenuPeople.querySelector(".popup__list-items");
+
+const mobileMenuServices = document.querySelector(".popup__list-services");
+const menuServices = mobileMenuServices.querySelector(".popup__button");
+const menuServicesBlock = mobileMenuServices.querySelector(".popup__list-items")
 
 /*Открытие попапа*/
 function openPopup(item) {
@@ -15,7 +25,30 @@ function openMobilePopup(item) {
     item.classList.toggle("popup__list-items_opened");
 }
 
+function openMenuMobile(item) {
+    item.classList.toggle("popup__button_opened");
+}
+
+function openMenuMobileBlock(item){
+    item.classList.toggle("popup__list-items_opened");
+}
+
 menuButton.addEventListener("click", function () {
     menuButton.classList.toggle("header-navigation__menu_opened");
-    openPopup(popup)
+    openPopup(popup);
+})
+
+menuProject.addEventListener("click", function (){
+    openMenuMobile(menuProject);
+    openMenuMobileBlock(menuProjectBlock);
+})
+
+menuPeople.addEventListener("click", function (){
+    openMenuMobile(menuPeople);
+    openMenuMobileBlock(menuPeopleBlock);
+})
+
+menuServices.addEventListener("click", function (){
+    openMenuMobile(menuServices);
+    openMenuMobileBlock(menuServicesBlock);
 })
